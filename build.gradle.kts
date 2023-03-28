ef955cc7013507d00157f32c2ada6ef79731737a
Johnny Miller (ZA) <johnnysviva@outlook.com>
2023-03-28 19:39:07 +0800

    build($Gradle): print unittest logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -39,4 +41,54 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// https://docs.gradle.org/current/userguide/performance.html#parallel_test_execution
+tasks.withType<Test>().configureEach {
+    // The normal approach is to use some number less than or equal to the number of CPU cores you have,
+    // such as this algorithm:
+    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
+    // To fork a new test VM after a certain number of tests have run
+    setForkEvery(100)
+}
+
+// https://docs.gradle.org/current/dsl/org.gradle.api.tasks.testing.Test.html
+tasks.withType<Test> {
+    // Configuration parameters to execute top-level classes in parallel but methods in the same thread
+    // https://www.jvt.me/posts/2021/03/11/gradle-speed-parallel/
+    systemProperties["junit.jupiter.execution.parallel.enabled"] = "true"
+    systemProperties["junit.jupiter.execution.parallel.mode.default"] = "concurrent"
+    systemProperties["junit.jupiter.execution.parallel.mode.classes.default"] = "concurrent"
+    // Discover and execute JUnit Platform-based tests
+    useJUnitPlatform()
+    failFast = true
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        // set options for log level LIFECYCLE
+        events = mutableSetOf(
+            FAILED,
+            PASSED,
+            SKIPPED,
+            STANDARD_OUT
+        )
+        exceptionFormat = FULL
+        showStandardStreams = true
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+        // set options for log level DEBUG and INFO
+        debug {
+            events = mutableSetOf(
+                FAILED,
+                PASSED,
+                SKIPPED,
+                STANDARD_OUT
+            )
+            exceptionFormat = FULL
+        }
+        info {
+            events = debug.events
+            exceptionFormat = debug.exceptionFormat
+        }
+    }
 }
